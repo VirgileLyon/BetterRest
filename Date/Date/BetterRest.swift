73bf20ec9f49c9fad315a59e1f86544a682ff7dd
VirgileLyon <45287046+VirgileLyon@users.noreply.github.com>
2021-04-21 11:39:43 +0200

    Add files via upload

---
+++ Date/Date/BetterRest.swift
@@ -0,0 +1,17 @@
+//
+//  DateApp.swift
+//  Date
+//
+//  Created by Virgile Vercellis on 14/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct DateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
